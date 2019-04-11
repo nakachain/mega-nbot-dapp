68329a4c766882ac7dca526a9e3ed93e540faaef
Deric Walintukan <deric.walintukan@gmail.com>
2019-04-11 12:12:00 +0700

    Add elements to meganbot page

--- src/pages/MegaNBOT/index.js
+++ src/pages/MegaNBOT/index.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { inject, observer } from 'mobx-react'
+import { Typography, Button, withStyles } from '@material-ui/core'
+import styles from './styles'
 import MegaNBOTMeta from '../../contracts/mega-nbot'
 import NBOTMeta from '../../contracts/nbot'
 import Constants from '../../constants'
 import Config from '../../config'
-import ContractInfo from '../../components/ContractInfo'
-import ContractAPI from '../../components/ContractAPI'
-import NotDeployedView from '../../components/NotDeployedView';
+import NotDeployedView from '../../components/NotDeployedView'
 
 const { NETWORK } = Constants
 const { TOKEN: { NBOT } } = Config
@@ -15,6 +15,11 @@ const { TOKEN: { NBOT } } = Config
 @inject('store')
 @observer
 class MegaNBOT extends Component {
+  static propTypes = {
+    classes: PropTypes.object.isRequired,
+    store: PropTypes.object,
+  }
+
   constructor(props) {
     super(props)
     this.state = {
@@ -22,6 +27,59 @@ class MegaNBOT extends Component {
       nbotContract: undefined,
       owner: undefined,
     }
+  }
+
+  renderRewardSection = () => {
+    const { classes } = this.props
+    const { winningAmount } = this.state
+    return (
+      <div className={classes.sectionContainer}>
+        <Typography variant="h5">
+          Current Drawing Reward:
+        </Typography>
+        <Typography variant="h4">
+          {winningAmount}
+        </Typography>
+      </div>
+    )
+  }
+
+  renderBlocksLeftSection = () => {
+    const { classes } = this.props
+    const { blocksLeft, estimatedTimeLeft } = this.state
+    return (
+      <div className={classes.sectionContainer}>
+        <Typography variant="h5">
+          Blocks Left:
+        </Typography>
+        <Typography variant="h4">
+          {blocksLeft}
+        </Typography>
+        <Typography variant="subtitle2">
+          approximately {estimatedTimeLeft}
+        </Typography>
+      </div>
+    )
+  }
+
+  renderEntrySection = () => {
+    const { classes } = this.props
+    return (
+      <div className={classes.sectionContainer}>
+        <Button title="Enter Drawing!" />
+      </div>
+    )
+  }
+
+  renderNoticeSection = () => {
+    const { classes } = this.props
+    return (
+      <div className={classes.sectionContainer}>
+        <Typography variant="h4">
+          New free drawings every day! Enter to win NBOT!
+        </Typography>
+      </div>
+    )
   }
 
   componentDidMount() {
@@ -67,139 +125,13 @@ class MegaNBOT extends Component {
 
     return (
       <div>
-        <ContractInfo
-          name="MegaNBOT"
-          owner={owner} />
-        <ContractAPI
-          title="Last Drawing Block Number"
-          description="Returns the last drawing block number."
-          buttonText="Get"
-          fields={[]}
-          contractMethod={contract && contract.lastDrawingBlockNum} />
-        <ContractAPI
-          title="Winning Amount"
-          description="Returns the winning amount for the drawing."
-          buttonText="Get"
-          fields={[]}
-          contractMethod={contract && contract.winningAmount}
-          responseFormat={{ symbol: NBOT.symbol, decimals: NBOT.decimals }} />
-        <ContractAPI
-          title="Drawing Interval"
-          description="Returns the minimum number of blocks to pass for each drawing."
-          buttonText="Get"
-          fields={[]}
-          contractMethod={contract && contract.withdrawInterval} />
-        <ContractAPI
-          title="Enter Drawing"
-          description="Enters the current drawing with the current address."
-          buttonText="Enter"
-          fields={[]}
-          contractMethod={contract && contract.enterDrawingFromSender} />
-        <ContractAPI
-          title="Enter Drawing & Deposit NBOT"
-          description="Enters the current drawing with the current address and deposits NBOT to the MegaNBOT contract."
-          buttonText="Enter"
-          fields={[
-            { label: 'to', value: (contract && contract.address) || '' },
-            { label: 'amount (NBOT)', decimals: NBOT.decimals },
-            { label: 'data', value: '0xeecb215f' },
-          ]}
-          contractMethod={nbotContract && nbotContract.transfer['address,uint256,bytes']} />
-        <ContractAPI
-          title="Deposit NBOT"
-          description="Deposits NBOT to the MegaNBOT contract."
-          buttonText="Deposit"
-          fields={[
-            { label: 'to', value: (contract && contract.address) || '' },
-            { label: 'amount (NBOT)', decimals: NBOT.decimals },
-            { label: 'data', value: '0xeecb215f' },
-          ]}
-          contractMethod={nbotContract && nbotContract.transfer['address,uint256,bytes']} />
-        <ContractAPI
-          title="Is In Current Drawing?"
-          description="Checks if the address is in the current drawing."
-          buttonText="Check"
-          fields={[
-            { label: 'address' },
-          ]}
-          contractMethod={contract && contract.isInCurrentDrawing} />
-        <ContractAPI
-          title="NBOT Address"
-          description="Returns the address of the NBOT contract."
-          buttonText="Get"
-          fields={[]}
-          contractMethod={contract && contract.nbotAddress} />
-        <ContractAPI
-          title="Locked?"
-          description="Returns if the contract is locked."
-          buttonText="Check"
-          fields={[]}
-          contractMethod={contract && contract.locked} />
-        <ContractAPI
-          title="Will Lock After Drawing?"
-          description="Returns if the contract will be locked after the current drawing."
-          buttonText="Check"
-          fields={[]}
-          contractMethod={contract && contract.lockAfterDrawing} />
-        {account === owner && (
-          <div>
-             <ContractAPI
-              title="Set Winning Amount"
-              description="Sets the winning amount."
-              buttonText="Set"
-              fields={[
-                { label: 'amount (NBOT)', decimals: NBOT.decimals },
-              ]}
-              contractMethod={contract && contract.setWinningAmount}
-              onlyOwner />
-            <ContractAPI
-              title="Set Drawing Interval"
-              description="Sets the minimum number of blocks for each drawing."
-              buttonText="Set"
-              fields={[
-                { label: 'interval (blocks)' },
-              ]}
-              contractMethod={contract && contract.setWithdrawInterval}
-              onlyOwner />
-            <ContractAPI
-              title="Lock Contract"
-              description="Locks the contract after the current drawing."
-              buttonText="Lock"
-              fields={[]}
-              contractMethod={contract && contract.lock}
-              onlyOwner />
-            <ContractAPI
-              title="Unlock Contract"
-              description="Unlocks the contract and starts a new drawing round. Contract must be locked."
-              buttonText="Unlock"
-              fields={[]}
-              contractMethod={contract && contract.unlock}
-              onlyOwner />
-            <ContractAPI
-              title="Withdraw Remaining NBOT"
-              description="Withdraws the remaining NBOT to the owner. Contract must be locked."
-              buttonText="Withdraw"
-              fields={[]}
-              contractMethod={contract && contract.withdraw}
-              onlyOwner />
-            <ContractAPI
-              title="Set NBOT Address"
-              description="Sets the NBOT contract address."
-              buttonText="Set"
-              fields={[
-                { label: 'address' },
-              ]}
-              contractMethod={contract && contract.setNBOTAddress}
-              onlyOwner />
-          </div>
-        )}
+        {this.renderRewardSection()}
+        {this.renderBlocksLeftSection()}
+        {this.renderEntrySection()}
+        {this.renderNoticeSection()}
       </div>
     )
   }
 }
 
-MegaNBOT.propTypes = {
-  store: PropTypes.object,
-}
-
-export default MegaNBOT
+export default withStyles(styles)(MegaNBOT)
---
+++ src/pages/MegaNBOT/styles.js
@@ -0,0 +1,5 @@
+export default theme => ({
+  sectionContainer: {
+    marginBottom: theme.spacing.five,
+  },
+})
