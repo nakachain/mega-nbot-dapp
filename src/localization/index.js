53129da39a7af5e2f2fba2dd30b686458a791a11
Deric Walintukan <deric.walintukan@gmail.com>
2019-04-16 14:37:56 +0700

    Default to eng if no translations found

--- src/localization/index.js
+++ src/localization/index.js
@@ -2,18 +2,28 @@ import { addLocaleData } from 'react-intl'
 import localeEn from 'react-intl/locale-data/en'
 import localeZh from 'react-intl/locale-data/zh'
 import messagesEn from './en.json'
+import messagesZh from './zh.json'
+import logger from '../utils/logger'
 
 // Load locale data
 addLocaleData([...localeEn, ...localeZh])
 
 const messages = {
   en: messagesEn,
+  zh: messagesZh,
 }
 
 export default () => {
-  const language = window.navigator.language.split(/[-_]/)[0]
+  let lang = window.navigator.language.split(/[-_]/)[0]
+
+  // Default to English if no messages file found for the language
+  if (!messages[lang]) {
+    logger.error(`No translations found for locale: ${lang}`)
+    lang = 'en'
+  }
+
   return {
-    locale: language,
-    messages: messages[language],
+    locale: lang,
+    messages: messages[lang],
   }
 }
